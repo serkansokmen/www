f431b938e5363bc75aee5ae03c7ac47d000d070d
Serkan Sokmen <e.serkan.sokmen@gmail.com>
2017-05-27 05:55:41 +0300

    cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,61 +17,27 @@ export class AppComponent implements OnInit {
 
   public linkItems;
   public currentStoryPath: string;
-  private timer: any;
-  private canSwitch: boolean;
 
   constructor(
     private stories: StoriesService,
     private socialLinks: SocialLinksService
   ) {
 
-    this.canSwitch = true;
     Observable
       .fromPromise(socialLinks.getSocialLinks())
       .subscribe(data => {
         this.linkItems = data;
       });
 
-    this.loop();
-  }
-
-  loop() {
-    this.timer = setTimeout((event) => {
-      console.log(event);
-    })
     Observable.of(null)
       .concatMap(() => Observable.timer(Math.random() * Math.floor(Math.random()) * 100))
       .do(() => this.currentStoryPath = this.stories.getNext())
       .repeat()  // optionally .repeat(10)
       .subscribe();
-
-
-
-    // .interval(40)
-    //   .take(this.stories.getStoryCount())
-    //   .map((x) => x)
-    //   .subscribe((x) => {
-    //     if (this.canSwitch) {
-    //       this.currentStoryPath = this.stories.getStoryPath(x);
-    //     }
-    //     if (this.currentStoryPath == this.stories[this.stories.getStoryCount()]) {
-    //       this.loop();
-    //     }
-    //   });
   }
 
   ngOnInit() {
     this.currentStoryPath = this.stories.getNext();
-  }
-
-  @HostListener('mousedown', ['$event'])
-  handleMousePress(event: MouseEvent) {
-    this.canSwitch = false;
-  }
-
-  @HostListener('document:mouseup', ['$event'])
-  handleMouseMove(event) {
-    this.canSwitch = true;
   }
 
 }

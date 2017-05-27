bd33ffa3aa217a5e240cfc5d87269a972fd2cdc9
Serkan Sokmen <e.serkan.sokmen@gmail.com>
2017-05-27 05:09:02 +0300

    lets go fast

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,23 +17,46 @@ export class AppComponent implements OnInit {
 
   public linkItems;
   public currentStoryPath: string;
+  private timer: any;
+  private canSwitch: boolean;
 
   constructor(
     private stories: StoriesService,
     private socialLinks: SocialLinksService
   ) {
 
+    this.canSwitch = true;
     Observable
       .fromPromise(socialLinks.getSocialLinks())
       .subscribe(data => {
         this.linkItems = data;
       });
 
-    // Observable.interval(40000)
+    this.loop();
+  }
+
+  loop() {
+    this.timer = setTimeout((event) => {
+      console.log(event);
+    })
+    Observable.of(null)
+      .concatMap(() => Observable.timer(Math.random() * Math.floor(Math.random()) * 100))
+      .do(() => this.currentStoryPath = this.stories.getNext())
+      .repeat()  // optionally .repeat(10)
+      .subscribe();
+
+
+
+    // .interval(40)
     //   .take(this.stories.getStoryCount())
-    //   .map((x) => x+1)
+    //   .map((x) => x)
     //   .subscribe((x) => {
-    //     this.currentStoryPath = this.stories.getStoryPath(x);
+    //     if (this.canSwitch) {
+    //       this.currentStoryPath = this.stories.getStoryPath(x);
+    //     }
+    //     if (this.currentStoryPath == this.stories[this.stories.getStoryCount()]) {
+    //       this.loop();
+    //     }
     //   });
   }
 
@@ -43,21 +66,12 @@ export class AppComponent implements OnInit {
 
   @HostListener('mousedown', ['$event'])
   handleMousePress(event: MouseEvent) {
-    this.currentStoryPath = this.stories.getNext();
+    this.canSwitch = false;
   }
 
-  @HostListener('document:keydown', ['$event'])
-  handleKeyboardEvent(event: KeyboardEvent) {
-    // left = 37
-    // up = 38
-    // right = 39
-    // down = 40
-    // space = 32
-    if (event.keyCode == 32 || event.keyCode == 39 || event.keyCode == 38) {
-      this.currentStoryPath = this.stories.getNext();
-    } else if (event.keyCode == 37 || event.keyCode == 40) {
-      this.currentStoryPath = this.stories.getPrevious();
-    }
+  @HostListener('document:mouseup', ['$event'])
+  handleMouseMove(event) {
+    this.canSwitch = true;
   }
 
 }
